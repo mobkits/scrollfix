83e03ddd7efcdad24bc707895d0eb649f5176a8b
chemzqm <chemzqm@gmail.com>
2014-05-26 20:01:52 +0800

    remove event listener when dom removed

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
+var events = require('event');
+var removed = require('removed');
 
 //not draggable
 document.addEventListener('touchmove', function(e) {
   e.preventDefault();
 });
 
+function ontouchmove(e) {
+  e.stopPropagation();
+}
+
+function ontouchstart(e) {
+  var el = e.currentTarget;
+  if (el.scrollTop === 0) {
+    el.scrollTop = 1;
+  } else if (el.scrollHeight == el.scrollTop + el.offsetHeight) {
+    el.scrollTop = el.scrollTop - 1;
+  }
+}
+
 module.exports = function (el) {
-  //it could be scrollable;
-  el.addEventListener('touchmove', function(e) {
-    e.stopPropagation();
+  events.bind(el, 'touchmove', ontouchmove);
+  events.bind(el, 'touchstart', ontouchstart);
+  removed(el, function () {
+    events.unbind(el, 'touchmove', ontouchmove);
+    events.unbind(el, 'touchstart', ontouchstart);
   })
-
-  var sy = 0;
-  //prevent dragging at top
-  el.addEventListener('touchstart', function(e) {
-    sy = e.pageY;
-    if (el.scrollTop === 0) {
-      el.scrollTop = 1;
-    } else if (el.scrollHeight == el.scrollTop + el.offsetHeight) {
-      el.scrollTop = el.scrollTop - 1;
-    }
-  })
-
 }
